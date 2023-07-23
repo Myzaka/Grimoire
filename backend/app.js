56a9c3f1d82d736a7c3461913667ba11abd1219c
Jerome <jerome.semik@bbox.fr>
2023-07-23 12:15:09 +0200

    Connexion base MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://compte_technique:53bPTZjHhtE%40@cluster0.uqhoutf.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
