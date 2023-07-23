9e82ed7cd5b2e96a7e48c0c82fee767431279a8c
Jerome <jerome.semik@bbox.fr>
2023-07-23 21:39:49 +0200

    Préparation création utilisateurs (manque bcrypt)

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://compte_technique:53bPTZjHhtE%40@cluster0.uqhoutf.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
