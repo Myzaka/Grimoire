2a78ed501fd222d3a0a28eed39a7abeef267d4f3
Jerome <jerome.semik@bbox.fr>
2023-09-17 16:06:52 +0200

    Test format image à l'upload

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -6,6 +6,7 @@ module.exports = multer({storage: storage}).single('image');*/
 
 
 const multer = require('multer');
+const mime = require('mime-types'); // Importez le module mime-types
 
 const storage = multer.memoryStorage();
 
@@ -23,8 +24,18 @@ module.exports = (req, res, next) => {
             // Une autre erreur s'est produite.
             return res.status(500).json({ error: 'Une erreur s\'est produite lors du téléchargement du fichier' });
         }
-        
-        // Si nous arrivons ici, cela signifie qu'un fichier a été téléchargé avec succès.
-        next();
+
+        // Vérifier le type MIME du fichier
+        const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp', 'image/avif'];
+        const fileType = mime.lookup(req.file.originalname);
+
+        if (allowedMimeTypes.includes(fileType)) {
+            // Le fichier est d'un type autorisé
+            next();
+        } else {
+            // Le fichier n'est pas d'un type autorisé
+            return res.status(400).json({ error: 'Format de fichier non pris en charge' });
+        }
     });
 };
+
